2027764053b4db6ad83d195e0dced101faae0bea
Ada Phillips <ragwafire99@gmail.com>
2024-12-04 17:23:53 -0500

    fix(script): set_orion_config.sh: update ExecStart line in orion.service to handle SCREEN_ROTATION only. (#26)
    Co-authored-by: PaulGD03 <paulskapczyk@gmail.com>

--- assets/scripts/set_orion_config.sh
+++ assets/scripts/set_orion_config.sh
@@ -11,7 +11,9 @@ SERVICE_FILE="/etc/systemd/system/orion.service"
 LINES=$(cat $SERVICE_FILE)
 
 # Replace the ExecStart line
-LINES=$(echo "$LINES" | sed "s|^ExecStart=.*|ExecStart=flutter-pi ${SCREEN_ROTATION:+-r $SCREEN_ROTATION} --release /home/$BASE_USER/orion|")
+if [ -n "$SCREEN_ROTATION" ]; then
+    LINES=$(echo "$LINES" | sed "s|flutter-pi\s*\(-r\s*[0-9]*\)*|flutter-pi -r $SCREEN_ROTATION|")
+fi
 
 # Write the updated lines back to the service file
 echo "$LINES" | sudo tee $SERVICE_FILE > /dev/null
