efddebbe70a12b97368ec5459efb8ada0d3355fe
PaulGD03 <paulskapczyk@gmail.com>
2024-12-05 15:11:19 +0100

    chore(license): update copyright information and license terms to reflect Open Resin Alliance

--- lib/settings/update_screen.dart
+++ lib/settings/update_screen.dart
@@ -1,34 +1,34 @@
-// ignore_for_file: use_build_context_synchronously
-
 /*
 * Orion - Update Screen
-* Copyright (C) 2024 TheContrappostoShop
+* Copyright (C) 2024 Open Resin Alliance
 *
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
 *
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
+*     http://www.apache.org/licenses/LICENSE-2.0
 *
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 */
+
+// ignore_for_file: use_build_context_synchronously
 
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:orion/pubspec.dart';
+import 'package:orion/util/markdown_screen.dart';
+import 'package:orion/util/orion_config.dart';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
-import 'package:orion/pubspec.dart';
-import 'package:orion/util/markdown_screen.dart';
-import 'package:orion/util/orion_config.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class UpdateScreen extends StatefulWidget {
