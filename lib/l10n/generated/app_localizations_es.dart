98c9583cdf12d7a229a117b793c00924ae1e060d
PaulGD03 <paulskapczyk@gmail.com>
2025-08-02 16:56:28 +0200

    feat(core): upgrade Flutter version, upgrade l10n setup
    l10n will no longer use a synthetic package and instead use the regular setup, removing the need for manual generation, and helping it play nicely with newer versions of flutter

---
+++ lib/l10n/generated/app_localizations_es.dart
@@ -0,0 +1,209 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get sectionCommon => 'Común';
+
+  @override
+  String get commonNext => 'Siguiente';
+
+  @override
+  String get commonBack => 'Atrás';
+
+  @override
+  String get commonSkip => 'Omitir';
+
+  @override
+  String get commonCompleteSetup => 'Completar Configuración';
+
+  @override
+  String get sectionSetup => 'Configuración';
+
+  @override
+  String get setupGetStarted => 'Comenzar';
+
+  @override
+  String get setupLanguageTitle => 'Seleccionar Idioma';
+
+  @override
+  String get setupRegionTitle => 'Seleccionar Región';
+
+  @override
+  String get setupTimezoneTitle => 'Seleccionar Zona Horaria';
+
+  @override
+  String get setupNameTitle => 'Elegir Nombre';
+
+  @override
+  String get setupThemeTitle => 'Seleccionar Tema';
+
+  @override
+  String get setupWifiTitle => 'Conectar a WiFi';
+
+  @override
+  String get setupCompleteTitle => '';
+
+  @override
+  String get sectionRegion => 'Región';
+
+  @override
+  String get regionSuggestedCountries => 'Países Sugeridos';
+
+  @override
+  String get regionAllCountries => 'Todos los Países';
+
+  @override
+  String get sectionTimezone => 'Zona Horaria';
+
+  @override
+  String get timezoneSuggested => 'Zonas Horarias Sugeridas';
+
+  @override
+  String get timezoneOther => 'Otras Zonas Horarias';
+
+  @override
+  String get timezoneNoneAvailable =>
+      'No hay zonas horarias disponibles para la región seleccionada';
+
+  @override
+  String get sectionPrinter => 'Impresora';
+
+  @override
+  String get printerName => 'Nombre de la Impresora';
+
+  @override
+  String get sectionTheme => 'Tema';
+
+  @override
+  String get themeDarkMode => 'Modo Oscuro';
+
+  @override
+  String get themeVendorLocked =>
+      'El tema ha sido establecido por el proveedor';
+
+  @override
+  String get themePurple => 'Morado';
+
+  @override
+  String get themeBlue => 'Azul';
+
+  @override
+  String get themeGreen => 'Verde';
+
+  @override
+  String get themeRed => 'Rojo';
+
+  @override
+  String get themeOrange => 'Naranja';
+
+  @override
+  String get themeColor => 'Color del Tema';
+
+  @override
+  String get sectionWifi => 'WiFi';
+
+  @override
+  String get wifiSkipTitle => '¿Omitir configuración WiFi?';
+
+  @override
+  String get wifiSkipMessage =>
+      'Siempre puedes conectarte a WiFi más tarde en ajustes';
+
+  @override
+  String get wifiConnectNow => 'Conectar Ahora';
+
+  @override
+  String get wifiSkipAnyway => 'Omitir de Todos Modos';
+
+  @override
+  String get wifiDisconnectTitle => '¿Desconectar del WiFi?';
+
+  @override
+  String get wifiDisconnectMessage =>
+      '¿Estás seguro de que quieres desconectarte de la red actual?';
+
+  @override
+  String get wifiStayConnected => 'Permanecer Conectado';
+
+  @override
+  String get wifiDisconnect => 'Desconectar';
+
+  @override
+  String get sectionComplete => 'Completado';
+
+  @override
+  String get setupCompletionMessage => '¡está listo para usar!';
+
+  @override
+  String get homePowerOptions => 'Opciones de Energía';
+
+  @override
+  String get homePowerRemote => '(Remoto)';
+
+  @override
+  String get homePowerLocal => '(Local)';
+
+  @override
+  String get homeFirmwareRestart => 'Reiniciar Firmware';
+
+  @override
+  String get homeRebootSystem => 'Reiniciar Sistema';
+
+  @override
+  String get homeShutdownSystem => 'Apagar Sistema';
+
+  @override
+  String get homeBtnPrint => 'Imprimir';
+
+  @override
+  String get homeBtnTools => 'Herramientas';
+
+  @override
+  String get homeBtnSettings => 'Ajustes';
+
+  @override
+  String get printUSBAvailable => 'USB Disponible';
+
+  @override
+  String get printUSBNotAvailable => 'USB No Disponible';
+
+  @override
+  String get printTitleInternal => 'Archivos de Impresión (Interno)';
+
+  @override
+  String get printTitleUSB => 'Archivos de Impresión (USB)';
+
+  @override
+  String get printTitleApiError => 'Error de API Odyssey';
+
+  @override
+  String get printTitleUSBError => '(USB)';
+
+  @override
+  String get printTitleInternalError => '(Interno)';
+
+  @override
+  String get printFetchFail => 'Error al obtener archivos';
+
+  @override
+  String get printNotPermitted => 'Operación no permitida';
+
+  @override
+  String get printSwitchUSB => 'Cambiar a USB';
+
+  @override
+  String get printUSBUnavailable => 'USB no disponible';
+
+  @override
+  String get printInternalSwitch => 'Cambiar a Interno';
+
+  @override
+  String get printParentDir => 'Directorio Superior';
+}
