6c919f8ac0a0e2d1b8da674aa0080bdf3df03feb
PaulGD03 <paulskapczyk@gmail.com>
2025-10-05 00:34:31 +0200

    feat(tests): Add timeout handling tests for OdysseyHttpClient and improve thumbnail utility tests

--- test/nanodlp/nanodlp_http_client_test.dart
+++ test/nanodlp/nanodlp_http_client_test.dart
@@ -1,3 +1,21 @@
+/*
+* Orion - NanoDLP HTTP Client Test
+* Copyright (C) 2025 Open Resin Alliance
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 
@@ -119,4 +137,30 @@ void main() {
       expect(plateRequests, 1);
     });
   });
+
+  group('NanoDlpHttpClient timeout', () {
+    test('getStatus fails fast when backend is unresponsive', () async {
+      final client = NanoDlpHttpClient(
+        clientFactory: () => _NeverCompletesClient(),
+        requestTimeout: const Duration(milliseconds: 25),
+      );
+
+      final sw = Stopwatch()..start();
+      final future = client.getStatus();
+      await expectLater(future, throwsA(isA<TimeoutException>()));
+      sw.stop();
+
+      expect(sw.elapsed, lessThan(const Duration(milliseconds: 300)));
+    });
+  });
+}
+
+class _NeverCompletesClient extends http.BaseClient {
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    return Completer<http.StreamedResponse>().future;
+  }
+
+  @override
+  void close() {}
 }
--- test/thumbnail_util_test.dart
+++ test/thumbnail_util_test.dart
@@ -1,5 +1,21 @@
+/*
+* Orion - Thumbnail Utility Test
+* Copyright (C) 2025 Open Resin Alliance
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -24,11 +40,19 @@ void main() {
     final path = await ThumbnailUtil.extractThumbnail('local', '', 'test.sl1',
         client: fake);
 
-    // The function returns a path; ensure file exists and contents match
+    // The function returns a path; ensure file exists and contents look like a PNG
     final f = File(path);
     expect(await f.exists(), isTrue);
     final bytes = await f.readAsBytes();
-    expect(bytes, equals(data));
+    // Thumbnail extraction produces an encoded image (PNG). Ensure we got
+    // non-empty bytes and that they start with the PNG signature.
+    expect(bytes, isNotEmpty);
+    expect(bytes.length, greaterThanOrEqualTo(8));
+    // PNG signature: 137 80 78 71 13 10 26 10
+    expect(bytes[0], equals(137));
+    expect(bytes[1], equals(80));
+    expect(bytes[2], equals(78));
+    expect(bytes[3], equals(71));
 
     // Clean up
     try {
