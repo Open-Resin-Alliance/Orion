ccd57b77a1c433f8ec9556569926032fabb9fc60
PaulGD03 <paulskapczyk@gmail.com>
2025-10-13 22:50:17 +0200

    feat(materials): add MaterialsScreen with navigation and placeholder content

---
+++ lib/materials/materials_screen.dart
@@ -0,0 +1,80 @@
+/*
+* Orion - Materials Screen
+* Copyright (C) 2025 Open Resin Alliance
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import 'package:flutter/material.dart';
+
+import 'package:orion/glasser/glasser.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class MaterialsScreen extends StatefulWidget {
+  const MaterialsScreen({super.key});
+
+  @override
+  MaterialsScreenState createState() => MaterialsScreenState();
+}
+
+class MaterialsScreenState extends State<MaterialsScreen> {
+  int _selectedIndex = 1;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassApp(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Materials'),
+        ),
+        body: _selectedIndex == 0
+            ? const Center(child: Text('To Be Implemented Soon!'))
+            : _selectedIndex == 1
+                ? const Center(child: Text('To Be Implemented Soon!'))
+                : const Center(child: Text('To Be Implemented Soon!')),
+        bottomNavigationBar: GlassBottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: PhosphorIcon(PhosphorIcons.flask()),
+              label: 'Resins',
+            ),
+            BottomNavigationBarItem(
+              icon: PhosphorIcon(PhosphorIcons.scales()),
+              label: 'Calibration',
+            ),
+            BottomNavigationBarItem(
+              icon: PhosphorIcon(PhosphorIcons.thermometer()),
+              label: 'Heaters',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          onTap: _onItemTapped,
+          unselectedItemColor: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+  }
+}
